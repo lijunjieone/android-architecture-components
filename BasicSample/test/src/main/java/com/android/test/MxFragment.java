6c04c267ab9cdc37da3fbe6ccf57fc737ed5429a
lijunjie <lijunjieone@gmail.com>
2017-08-24 14:09:54 +0800

    完成数据绑定的测试

--- BasicSample/test/src/main/java/com/android/test/MxFragment.java
+++ BasicSample/test/src/main/java/com/android/test/MxFragment.java
@@ -2,14 +2,15 @@ package com.android.test;
 
 import android.arch.lifecycle.LifecycleFragment;
 import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+
+import com.android.test.databinding.ListFragment2Binding;
+//import com.android.test.databinding.ListFragmentBinding;
 
 //import com.android.test.databinding.ListFragmentBinding;
 
@@ -20,16 +21,17 @@ import android.widget.FrameLayout;
 public class MxFragment extends LifecycleFragment {
 
     public static final String TAG=MxFragment.class.getSimpleName();
-//    ListFragmentBinding mBinding;
+    ListFragment2Binding mBinding;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
-//        mBinding = DataBindingUtil.inflate(inflater, R.layout.list_fragment, container, false);
-//        mBinding.setVariable()
-//        return mBinding.getRoot();
-        FrameLayout f=new FrameLayout(getContext());
-        f.setBackgroundColor(Color.BLUE);
-        return f;
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.list_fragment2, container, false);
+        mBinding.setIsLoading(false);
+        mBinding.getRoot().setBackgroundColor(Color.RED);
+        return mBinding.getRoot();
+//        FrameLayout f=new FrameLayout(getContext());
+//        f.setBackgroundColor(Color.BLUE);
+//        return f;
     }
 }
