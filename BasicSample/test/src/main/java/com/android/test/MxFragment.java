1136c9baeb9ac97201ad1f81db901f77637008be
lijunjie <lijunjieone@gmail.com>
2017-08-24 16:20:07 +0800

    测试一个简单的list的例子

--- BasicSample/test/src/main/java/com/android/test/MxFragment.java
+++ BasicSample/test/src/main/java/com/android/test/MxFragment.java
@@ -2,12 +2,15 @@ package com.android.test;
 
 import android.arch.lifecycle.LifecycleFragment;
 import android.databinding.DataBindingUtil;
+import android.databinding.adapters.TextViewBindingAdapter;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.android.test.databinding.ListFragment2Binding;
 //import com.android.test.databinding.ListFragmentBinding;
@@ -22,6 +25,8 @@ public class MxFragment extends LifecycleFragment {
 
     public static final String TAG=MxFragment.class.getSimpleName();
     ListFragment2Binding mBinding;
+
+    RecyclerView mListView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -29,9 +34,59 @@ public class MxFragment extends LifecycleFragment {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.list_fragment2, container, false);
         mBinding.setIsLoading(false);
         mBinding.getRoot().setBackgroundColor(Color.RED);
+
+        mListView = (RecyclerView)mBinding.getRoot().findViewById(R.id.products_list);
+        initView();
         return mBinding.getRoot();
 //        FrameLayout f=new FrameLayout(getContext());
 //        f.setBackgroundColor(Color.BLUE);
 //        return f;
     }
+
+
+    private void initView() {
+        mListView.setAdapter(new RecyclerView.Adapter<TextViewHolder>() {
+            @Override
+            public TextViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                TextView t= new TextView(getActivity());
+                t.setText("test");
+
+                return new TextViewHolder(t);
+
+            }
+
+            @Override
+            public void onBindViewHolder(TextViewHolder holder, int position) {
+                holder.setText("position:"+position);
+
+            }
+
+
+            @Override
+            public int getItemCount() {
+                return 30;
+            }
+        });
+    }
+
+
+    class TextViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mT;
+
+        public TextViewHolder(View itemView) {
+            super(itemView);
+            if(itemView instanceof TextView) {
+                mT=(TextView)itemView;
+            }
+        }
+
+        public void setText(String text) {
+            if(mT!=null) {
+                mT.setText(text);
+            }
+        }
+
+    }
+
 }
